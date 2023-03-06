23aa5c69881f288fc7cf363f8b5ffd732a4803b6
Ben Darwin <bcdarwin@gmail.com>
2023-03-06 15:17:33 -0500

    evaluate: add `cookiecutter` dependency to allow `evaluate-cli` to work

--- container/overlays/evaluate/default.nix
+++ container/overlays/evaluate/default.nix
@@ -25,6 +25,7 @@
 , pythonOlder
 , pythonRelaxDepsHook
 , pytestCheckHook
+, cookiecutter
 , datasets
 , dill
 , fsspec
@@ -58,6 +59,7 @@ buildPythonPackage rec {
   pythonRelaxDeps = [ "responses" ];
 
   propagatedBuildInputs = [
+    cookiecutter
     datasets
     numpy
     dill
