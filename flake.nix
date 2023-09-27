2f03e7f7163fbd17ad6f40334d6dd3984bbfb169
Ben Darwin <bcdarwin@gmail.com>
2023-09-27 11:33:19 -0400

    bump nixGL

--- flake.nix
+++ flake.nix
@@ -33,6 +33,7 @@
         "470.103.01"
         "470.161.03"
         "530.41.03"
+        "535.104.05"
       ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
