072fa7eb2b6373344c24f82f96e166b20ca5aa54
Ben Darwin <bcdarwin@gmail.com>
2023-09-27 14:12:10 -0400

    mk-docker.nix: set `created = "now" for convenience`

--- flake/mk-docker.nix
+++ flake/mk-docker.nix
@@ -6,8 +6,8 @@ with pkgs.dockerTools;
 buildImage {
   name = "test-docker";
   tag = "test";
+  created = "now";
   copyToRoot = contents;
   diskSize = 1024*80;
   buildVMMemorySize = 1024*8;
 }  
-    
