8caf09310da42eba440be22ec5eb30d11b7b49a4
Ben Darwin <bcdarwin@gmail.com>
2023-10-02 11:42:04 -0400

    flake.nix: switch from nixpkgs/master to nixpkgs/nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   # Nixpkgs / NixOS version to use.
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     #nixGL.url = "github:guibou/nixGL";
     nixGL.url = "github:cfhammill/nixGL";
     nixGL.inputs.nixpkgs.follows = "nixpkgs";
