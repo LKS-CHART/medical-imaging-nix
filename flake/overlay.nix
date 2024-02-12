0863ad8dfcf50384e04d3b20d9d7b0aeb6a4125f
Ben Darwin <bcdarwin@gmail.com>
2024-02-12 13:35:04 -0500

    overlay.nix: remove python311Packages.{pillow-jpls,highdicom} which have been upstreamed

--- flake/overlay.nix
+++ flake/overlay.nix
@@ -19,45 +19,6 @@
       pythonImportsCheck = [ "orthanc_xnat_tools" ];
     };
 
-    pillow-jpls = pfinal.buildPythonPackage rec {
-      pname = "pillow-jpls";
-      version = "1.2.0";
-      format = "wheel";
-
-      src = final.fetchurl {
-        url = "https://files.pythonhosted.org/packages/72/f3/725fd022d58e95374c0c6e8e3d183126938ffec580583fa2bf24a453191d/pillow_jpls-1.2.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl";
-        hash = "sha256-zpOAh+TPpO4YtleDfHXTtISpa3ibTbq9E7B4k2slhEA=";
-      };
-
-      propagatedBuildInputs = with pprev; [ pillow ];
-    };
-
-    highdicom = let
-      test_data = prev.fetchFromGitHub {
-        owner = "pydicom";
-        repo = "pydicom-data";
-        rev = "cbb9b2148bccf0f550e3758c07aca3d0e328e768";
-        hash = "sha256-nF/j7pfcEpWHjjsqqTtIkW8hCEbuQ3J4IxpRk0qc1CQ=";
-      }; in
-    pfinal.buildPythonPackage rec {
-      pname = "highdicom";
-      version = "0.22.0";
-      src = final.fetchFromGitHub {
-        owner = "ImagingDataCommons";
-        repo = "highdicom";
-        rev = "refs/tags/v${version}";
-        hash = "sha256-KHSJWEnm8u0xHkeeLF/U7MY4FfiWb6Q0GQQy2w1mnKw=";
-      };
-
-      preCheck = ''
-        export HOME=$TMP/test-home
-        mkdir -p $HOME/.pydicom/
-        ln -s ${test_data}/data_store/data $HOME/.pydicom/data
-      '';
-
-      propagatedBuildInputs = with pfinal; [ numpy pillow pillow-jpls pydicom ];
-      nativeCheckInputs = with pprev; [ pytestCheckHook ];
-    };
     ipyevents = pfinal.buildPythonPackage rec {
       pname = "ipyevents";
       version = "2.0.1";
