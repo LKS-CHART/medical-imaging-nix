287106de5a4098799f7fc4772a3832c67b4c89e0
cfhammill <christopher.hammill@unityhealth.to>
2024-02-26 14:48:52 -0500

    ablate deps for text work

--- flake/dependencies.nix
+++ flake/dependencies.nix
@@ -24,181 +24,44 @@ in
   watch
   openssl
   bashInteractive
-  git gdown git-annex
+  git
   openssh
   less
   which
   curl wget
   zip unzip
-  pigz  # for dcm2niix
   pandoc quarto
-  dcm2niix dcmtk gdcm ants
   parallel
   ruff
   cudaPackages.cudatoolkit
   ] ++ glWrappers ++ [
   autoGlWrapper
-  (emacsWithPackages (ps: with ps; [ magit ess poly-R elpy nix-mode ]))
-  (with rPackages;
-    # rWrapper bakes R_SITE_LIBS into the intepretter
-    rWrapper.override {
-      packages = [
-        R
-        batchtools
-        data_table
-        forecast
-        tidyverse
-        tidymodels
-        reticulate
-        #charticles
-        keyring
-        dbplyr
-        RMariaDB
-        pins
-        pROC
-        distr
-        lenses
-        openxlsx
-        vetiver
-        wrapr
-      ];
-    })
-    (python311.withPackages (ps: with ps; [
-        accelerate
-        addict
-        albumentations
-        #apex
-        #apiron
-        argon2_cffi
-        argon2-cffi-bindings
-        #arviz
-        asn1crypto
-        async_generator
-        attrs
-        avro
-        backcall
-        batchgenerators
-        bleach
+  (emacsWithPackages (ps: with ps; [ magit ess elpy nix-mode ]))
+  (python311.withPackages (ps: with ps; [
         bitsandbytes
-        bokeh
-        #catboost
-        certifi
-        cffi
-        chardet
-        cloudpickle
-        cryptography
-        cycler
-        cython
         dask
         datasets
-        dicom2nifti
-        #debugpy
-        decorator
-        defusedxml
         deid
-        #efficientnet-pytorch
-        entrypoints
-        evaluate
-        einops
         grad-cam
-        #fastai
-        fire  # for monai.bundle
-        greenlet
-        h5py
-        highdicom
-        httplib2
-        hupper
-        idna
-        ignite
-        imageio
-        importlib-metadata
-        ipykernel
-        ipython
-        ipyniivue
-        ipympl
-        ipyannotations
-        ipywidgets
-        superintendent
-        nibabel
-        nipype
-        antspyx
-        iso8601
-        jedi
         jinja2
-        joblib
-        json5
-        jsonschema
         jupyter
-        jupyter-client
-        jupyter_console
-        jupyterlab-pygments
-        jupyterlab-widgets
-        kafka-python
         kaggle
         keyring
         keyrings-cryptfile
-        kiwisolver
-        lightgbm
-        #llvmlite
-        markupsafe
         matplotlib
-        mistune
         mlflow
-        monai
-        monai-deploy  # n.b.: breaks monai without highdicom
-        msgpack
-        munch
-        mysql-connector
-        mdai
-        nbclient
-        nbconvert
         nbdev
         nbformat
-        nest-asyncio
         networkx
-        notebook
         numpy
-        oauthlib
         openpyxl
-        #orthanc-rest-client
-        orthanc-xnat-tools
-        packaging
         pandas
-        pandocfilters
-        parso
-        PasteDeploy
-        pbkdf2
         peft
-        pexpect
-        pgnotify
-        psycopg2
-        pickleshare
+        psycopg
         pillow
         pip
-        plaster
-        plaster-pastedeploy
-        prometheus-client
-        prompt-toolkit
-        protobuf
-        ptyprocess
-        pudb
-        pycm
-        pycparser
-        pycrypto
-        pydicom
-        pydicom-seg
-        dicomweb-client
-        pynetdicom
-        pygments
         pynvml
-        py-spy
         pyodbc
-        pyorthanc
-        pyparsing
-        #pypx
-        pyramid
-        #pyramid_mailer
-        pyrsistent
         pytest
         pytest-dotenv
         pytest-postgresql
@@ -209,56 +72,20 @@ in
         pytz
         pywavelets
         pyxdg
-        pyxnat
         pyyaml
-        pyzmq
-        qtconsole
-        qtpy
         requests
-        requests-cache
-        requests_oauthlib
-        rpy2
         safetensors
-        scikitimage
         scikit-learn
         scipy
-        secretstorage
-        send2trash
-        sqlalchemy
-        terminado
-        terminaltables
-        testpath
-        text-unidecode
         polars
         pydantic
-        pyradiomics
         torch
         pytorch-lightning
-        rising
-        simpleitk
         seaborn
-        statsmodels
         tensorboard
         tensorboardx
-        torchvision
         torchmetrics
-        timm
-        torchio
         tqdm
-        traitlets
-        transaction
         transformers
-        translationstring
-        ttach
-        typing-extensions
-        urllib3
-        urwid
-        venusian
-        wcwidth
-        webencodings
-        widgetsnbextension
-        wtforms
-        xnatpy
-        zipp
     ]))
 ]
