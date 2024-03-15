cbd5396e5366c6f6c56889ee20d7765156bec49f
cfhammill <christopher.hammill@unityhealth.to>
2024-03-15 16:32:09 -0400

    build-and-tag: script + error check

---
+++ scripts/build-and-tag-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+if [[ -z $(docker images -q medical-imaging-nix:"$(git rev-parse --short=8 HEAD)") ]]; then
+   nix build .\#docker && \
+     docker image load -i result && \
+     docker image tag test-docker:test medical-imaging-nix:"$(git rev-parse --short=8 HEAD)"
+else 
+     echo Image at tag medical-imaging-nix:"$(git rev-parse --short=8 HEAD)" already exists
+fi
+
